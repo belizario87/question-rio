152b9781688b1464b9744a88459c9d7416c49e73
belizario87 <leonardobelizario88@gmail.com>
2025-06-04 19:45:53 -0300

    feat: update database connection name to 'perguntasrio'

--- database/database.js
+++ database/database.js
@@ -1,11 +1,11 @@
 const Sequelize = require("sequelize");
 
-const connection = new Sequelize("database", "username", "password", {
+const connection = new Sequelize("perguntasrio", "username", "password", {
   host: "localhost",
   dialect: "mysql",
 });
 
 module.exports = connection;
 
-// Note: Replace "database", "username", and "password" with your actual database credentials.
+// Note: Replace "perguntasrio", "username", and "password" with your actual database credentials.
 // Ensure you have the MySQL server running and the database created before running this code.
